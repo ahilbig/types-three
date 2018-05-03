62b5667b36feb697d880f35c6a9288a50854a709
Andreas Hilbig <ahilbig@me.com>
2018-05-03 10:17:53 +0200

    Included webvr dependency + recursive import of dependant type

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,6 @@
-// Type declarations for three.js samples
-import {Camera, Object3D} from "three";
+// Type declarations for three.js, extended by type definitions for samples
+export * from "three";
 
-export declare namespace THREE {
-    export class DragControls {
-        constructor(objects, camera: Camera, domElement?: HTMLElement);
+export * from "./three-dragcontrols"
 
-        object: Camera;
-        domElement: HTMLElement | HTMLDocument;
-
-        activate(): void;
-
-        deactivate(): void;
-
-        dispose(): void;
-
-        // EventDispatcher mixins
-        addEventListener(type: string, listener: (event: Event) => void ): void;
-        hasEventListener(type: string, listener: (event: Event) => void): void;
-        removeEventListener(type: string, listener: (event: Event) => void): void;
-        dispatchEvent(event: { type: string; [attachment: string]: any; }): void;
-    }
-}
+export as namespace THREE;
---
+++ three-dragcontrols.d.ts
@@ -0,0 +1,21 @@
+export class DragControls {
+  constructor(objects, camera: Camera, domElement?: HTMLElement);
+
+  object: Camera;
+  domElement: HTMLElement | HTMLDocument;
+
+  activate(): void;
+
+  deactivate(): void;
+
+  dispose(): void;
+
+  // EventDispatcher mixins
+  addEventListener(type: string, listener: (event: Event) => void): void;
+
+  hasEventListener(type: string, listener: (event: Event) => void): void;
+
+  removeEventListener(type: string, listener: (event: Event) => void): void;
+
+  dispatchEvent(event: { type: string; [attachment: string]: any; }): void;
+}
